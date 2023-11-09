12efe131e833968a1b41c10303506b48fba901cb
YagoAkio <yagokuniyoshi1@gmail.com>
2023-11-09 10:10:38 -0300

    https://github.com/YagoAkio/SisDentalFx

--- src/main/java/br/fipp/sisdentalfx/ProcedimentoViewController.java
+++ src/main/java/br/fipp/sisdentalfx/ProcedimentoViewController.java
@@ -1,37 +1,40 @@
 package br.fipp.sisdentalfx;
 
-import br.fipp.sisdentalfx.db.dals.MaterialDAL;
-import br.fipp.sisdentalfx.db.entidades.Material;
+import br.fipp.sisdentalfx.db.dals.ProcedimentoDAL;
+import br.fipp.sisdentalfx.db.entidades.Procedimento;
 import br.fipp.sisdentalfx.db.util.DB;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
-import javafx.scene.control.*;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Control;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class MaterialViewController implements Initializable {
+public class ProcedimentoViewController  implements Initializable {
     public TextField tfID;
-    public TextField tfPreco;
     public TextField tfDesc;
+    public TextField tfTempo;
     public Button btnAceitar;
     public Button btnRecusar;
+    public TextField tfValor;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         Platform.runLater(()-> tfID.requestFocus());
         btnRecusar.setOnAction(e->{((Control)e.getSource()).getScene().getWindow().hide();});
-        btnAceitar.setOnAction(e->gravarMaterial(e));
-
-
+        btnAceitar.setOnAction(e->gravarProcedimento(e));
     }
 
-    private void gravarMaterial(ActionEvent e) {
-        Material material;
+    private void gravarProcedimento(ActionEvent e) {
+        Procedimento procedimento;
         String id = tfID.getText();
         String desc = tfDesc.getText();
-        String preco = tfPreco.getText();
+        String preco = tfValor.getText();
+        String tempo = tfTempo.getText();
 
 
         if (id.isEmpty() || desc.isEmpty() || preco.isEmpty()) {
@@ -46,7 +49,7 @@ public class MaterialViewController implements Initializable {
             Preco = Double.parseDouble(preco);
         } catch (NumberFormatException ex) {
             Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setContentText("O Preco deve ser um número válido.");
+            alert.setContentText("O Valor deve ser um número válido.");
             alert.showAndWait();
             return;
         }
@@ -56,23 +59,33 @@ public class MaterialViewController implements Initializable {
             Id = Integer.parseInt(id);
         } catch (NumberFormatException ex) {
             Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setContentText("O Preco deve ser um número válido.");
+            alert.setContentText("O ID deve ser um número válido.");
             alert.showAndWait();
             return;
         }
 
-        material = new Material(Id, desc, Preco);
+        int Tempo;
+        try {
+            Tempo = Integer.parseInt(tempo);
+        } catch (NumberFormatException ex) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("O Tempo deve ser um número válido.");
+            alert.showAndWait();
+            return;
+        }
 
-        MaterialDAL materialDAL = new MaterialDAL();
-        boolean gravacaoComSucesso = materialDAL.gravar(material);
+        procedimento = new Procedimento(Id, desc, Tempo, Preco);
+
+        ProcedimentoDAL procedimentoDAL = new ProcedimentoDAL();
+        boolean gravacaoComSucesso = procedimentoDAL.gravar(procedimento);
 
         if (!gravacaoComSucesso) {
             Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setContentText("Erro ao gravar o novo material: " + DB.getCon().getMensagemErro());
+            alert.setContentText("Erro ao gravar o novo procedimento: " + DB.getCon().getMensagemErro());
             alert.showAndWait();
         } else {
             Alert alert = new Alert(Alert.AlertType.INFORMATION);
-            alert.setContentText("Material gravado com sucesso!");
+            alert.setContentText("Procedimento gravado com sucesso!");
             alert.showAndWait();
         }
 
